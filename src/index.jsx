f2dd021b3d05a34d9351bb927a32bdc235780d77
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-31 16:33:32 -0600

    Modified 'index.jsx' -component- Added the redux store and provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // This provider will provide the global state to our app
+import store from './redux/store'; // This is our Redux store
 
 import App from './App';
 
@@ -7,7 +9,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 );
