f4f0d18ccad9a970350866aa55cec7c10cadadd9
Ruthmy <Ruryemo.exe@gmail.com>
2023-08-02 19:38:18 -0600

    Created 'weatherAPI.js' -file- to manage the key and urls from the APIs

---
+++ src/API/weatherAPI.js
@@ -0,0 +1,11 @@
+// Purpose: AccuWeather API key and base URL for weather data
+
+const weatherAPI = {
+  key: 'bAjmE9oXvW9VDxH5HR4Fhgm7rBZEdhmh',
+  baseURL: 'http://dataservice.accuweather.com/',
+  regionList: 'locations/v1/regions',
+  locationsTopCitiesURL: 'locations/v1/topcities/150',
+  currentConditionsURL: 'currentconditions/v1/',
+};
+
+export default weatherAPI;
