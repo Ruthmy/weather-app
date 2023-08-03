67b3bee88ea2765aa357bcbd87e630ab25ac4d50
Ruthmy <Ruryemo.exe@gmail.com>
2023-08-03 14:25:10 -0600

    Modified 'Home.jsx' -component- updated handling of the API data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/Home.css';
 import { useSelector } from 'react-redux';
+
+import continentsImages from '../assets/bd/continentsImages';
+import regionListArray from '../assets/bd/regionList';
+
+import '../styles/Home.css';
 
 const Home = () => {
   // Get the continents from the global state
@@ -16,26 +20,45 @@ const Home = () => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/World_map_-_low_resolution.svg/1024px-World_map_-_low_resolution.svg.png"
           alt="Mapa Mundi"
         />
-        <div>
+        <div className="home__info">
           <h1 className="title">Countries of the World</h1>
-          <h2 className="description">
+          <p className="description">
             Click on a continent to see the countries of that continent.
-          </h2>
+          </p>
         </div>
       </div>
       <div className="continents">
-
-        {continents.map((continent) => (
-          <NavLink
-            to={`/${continent.EnglishName}`}
-            className="continent"
-            key={continent.EnglishName}
-            continent={continent.EnglishName}
-          >
-            {continent.EnglishName}
-          </NavLink>
-        ))}
-
+        {
+        // In case continents are empty (API bug, etc.)
+        // continents are rendered from the regionListArray variable
+          continents
+            ? regionListArray && regionListArray.map((region) => (
+              <NavLink
+                to={`/${region.EnglishName}`}
+                className="continents__link"
+                key={region.EnglishName}
+              >
+                <img
+                  className="continents__image"
+                  src={continentsImages[region.EnglishName]}
+                  alt={region.EnglishName}
+                />
+              </NavLink>
+            ))
+            : continents && continents.map((continent) => (
+              <NavLink
+                to={`/${continent.EnglishName}`}
+                className="continents__link"
+                key={continent.EnglishName}
+              >
+                <img
+                  className="continents__image"
+                  src={continentsImages[continent.EnglishName]}
+                  alt={continent.EnglishName}
+                />
+              </NavLink>
+            ))
+        }
       </div>
     </>
   );
