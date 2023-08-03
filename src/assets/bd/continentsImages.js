ff12edfb302824c51db31d81784372f4ccce965d
Ruthmy <Ruryemo.exe@gmail.com>
2023-08-03 12:30:25 -0600

    Created 'continentsImages.js' -file- Object with all the continent images

---
+++ src/assets/bd/continentsImages.js
@@ -0,0 +1,18 @@
+import AfricaContinent from '../img/Africa.svg';
+import AntarcticaContinent from '../img/Antarctica.svg';
+import AsiaContinent from '../img/Asia.svg';
+import EuropeContinent from '../img/Europe.svg';
+import OceaniaContinent from '../img/Oceania.svg';
+import AmericaContinent from '../img/America.svg';
+
+// Create an array of continents images
+const continentsImages = {
+  Africa: AfricaContinent,
+  Americas: AmericaContinent,
+  Antarctica: AntarcticaContinent,
+  Asia: AsiaContinent,
+  Europe: EuropeContinent,
+  Oceania: OceaniaContinent,
+};
+
+export default continentsImages;
