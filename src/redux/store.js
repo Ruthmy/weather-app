f2dcf5449294adf9dfcae995a41984db6048440e
Ruthmy <Ruryemo.exe@gmail.com>
2023-08-04 00:41:19 -0600

    Modified 'store.js' -Redux Store- deleted unnecessary reducers.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import locationReducer from './countries/LocationSlice';
 import weatherReducer from './weather/weatherSlice';
 
 const store = configureStore({
   reducer: {
     weather: weatherReducer,
-    location: locationReducer,
   },
 });
 
