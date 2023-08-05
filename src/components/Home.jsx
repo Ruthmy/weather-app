0c897b45e0a247b2c3fa2154e6b31ee72a563c19
Ruthmy <Ruryemo.exe@gmail.com>
2023-08-05 02:29:35 -0600

    Modified 'Home.jsx' -component- Improved the use of the useSelector hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import continentsImages from '../assets/bd/continentsImages';
 import regionListArray from '../assets/bd/regionList';
@@ -7,43 +8,53 @@ import regionListArray from '../assets/bd/regionList';
 import logo from '../assets/img/logo.svg';
 import '../styles/Home.css';
 
-const Home = () => (
-  <>
-    <div className="home d-flex-row">
-      <img
-        className="header"
-        src={logo}
-        alt="Weather App Logo"
-      />
-      <div className="home__info">
-        <h1 className="title">WEATHER APP</h1>
-        <p className="description">
-          listed cities.
-        </p>
-      </div>
-    </div>
-    <div className="division"><h2>CONTINENTS</h2></div>
-    <div className="continents d-flex-row">
-      {regionListArray && regionListArray.map((region) => (
-        <NavLink
-          to={`/${region.LocalizedName}`}
-          className="continents__link d-flex-column"
-          key={region.EnglishName}
-        >
-          <img
-            className="continents__image"
-            src={continentsImages[region.EnglishName]}
-            alt={region.EnglishName}
-          />
-          <p
-            className="continents__detail"
-          >
-            {region.EnglishName}
+const Home = () => {
+  const citiesPerContinent = useSelector((state) => state.weather.weather);
+  return (
+    <>
+      <div className="home d-flex-row">
+        <img
+          className="header"
+          src={logo}
+          alt="Weather App Logo"
+        />
+        <div className="home__info">
+          <h1 className="title">WEATHER APP</h1>
+          <p className="description">
+            {citiesPerContinent.length > 0
+              ? ` We currently have ${citiesPerContinent.length} cities listed.`
+              : ' We currently have no cities listed.'}
           </p>
-        </NavLink>
-      ))}
-    </div>
-  </>
-);
+        </div>
+      </div>
+      <div className="division"><h2>CONTINENTS</h2></div>
+      <div className="continents d-flex-row">
+        {regionListArray && regionListArray.map((region) => (
+          <NavLink
+            to={`/${region.LocalizedName}`}
+            className="continents__link d-flex-column"
+            key={region.EnglishName}
+          >
+            <img
+              className="continents__image"
+              src={continentsImages[region.EnglishName]}
+              alt={region.EnglishName}
+            />
+            <p className="continents__detail">
+              {region.EnglishName}
+            </p>
+            <p className="continents__cities">
+              {
+                citiesPerContinent.filter(
+                  (city) => city.TimeZone.Name.includes(region.LocalizedName),
+                ).length
+              }
+            </p>
+          </NavLink>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
